6e74630c8d715abb738b7cc3285de92206fd4d27
Angela Adams <aadams3867@gmail.com>
2016-10-15 04:27:46 -0400

    Fixes db seeds for Heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-USE burgers_db;
-
 INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Big Mac', 0);
 INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Whopper', 0);
 INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Teriyaki Chicken', 0);
