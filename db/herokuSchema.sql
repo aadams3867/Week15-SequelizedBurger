5b3abbbe5169cfb37dcb0f07e37abb55b75ecc65
Angela Adams <aadams3867@gmail.com>
2016-10-14 22:44:58 -0400

    initial commit

---
+++ db/herokuSchema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE burgers
+(
+	`id` int AUTO_INCREMENT NOT NULL,
+	`burger_name` varchar(50) NOT NULL,
+	`devoured` tinyint(1) DEFAULT 0,
+	`date` timestamp(0) DEFAULT CURRENT_TIMESTAMP,
+    `createdAt` timestamp NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY(id)
+);
