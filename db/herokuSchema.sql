90e67112f57351611074bf1edc1e1132501d661b
Angela Adams <aadams3867@gmail.com>
2016-10-17 13:58:20 -0400

    Fixes capitalization issue with Heroku

--- db/herokuSchema.sql
+++ db/herokuSchema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE burgers
+CREATE TABLE Burgers
 (
 	`id` int AUTO_INCREMENT NOT NULL,
 	`burger_name` varchar(50) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Big Mac', 0);
-INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Whopper', 0);
-INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Teriyaki Chicken', 0);
-INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Double-Double, Animal-Style', 0);
-INSERT INTO burgers (`burger_name`, `devoured`) VALUES ('Beef and Cheddar', 0);
+INSERT INTO Burgers (`burger_name`, `devoured`) VALUES ('Big Mac', 0);
+INSERT INTO Burgers (`burger_name`, `devoured`) VALUES ('Whopper', 0);
+INSERT INTO Burgers (`burger_name`, `devoured`) VALUES ('Teriyaki Chicken', 0);
+INSERT INTO Burgers (`burger_name`, `devoured`) VALUES ('Double-Double, Animal-Style', 0);
+INSERT INTO Burgers (`burger_name`, `devoured`) VALUES ('Beef and Cheddar', 0);
